217bef3bb3edff1d7678607c8f9dfbfa89bb9b66
Maxwellsmile <1075808538@qq.com>
2017-08-15 17:35:30 +0800

    fspos1.js

--- zhou/fspos.js
+++ zhou/fspos.js
@@ -12,12 +12,12 @@ function getInput(intfile){//同步读文件
 function output(temp,outfile){//写文件
     var fs=require('fs');
     fs.writeFile(outfile,temp,function(err){
-      if(err){
-        console.log(err)
-       }
-      else{
-	console.log('ok')
-      }
+        if(err){
+            console.log(err)
+        }
+        else{
+	    console.log('ok')
+        }
     });
 }
 
@@ -25,7 +25,7 @@ function output(temp,outfile){//写文件
 function printInventory() {
     var dateDigitToString = function (num) {
     	return num < 10 ? '0' + num : num;//如果小于10就加个0,否则直接输出num;
-   	};
+    };
     var currentDate = new Date(),
             year = dateDigitToString(currentDate.getFullYear()),//获取年份
             month = dateDigitToString(currentDate.getMonth() + 1),//获取月份,从零开始算,所以加1;
@@ -34,7 +34,7 @@ function printInventory() {
             minute = dateDigitToString(currentDate.getMinutes()),//获取分钟;
             second = dateDigitToString(currentDate.getSeconds()),//获取秒;
             formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
-     var temp="***<没钱赚商店>购物清单***\n"+'打印时间:' + formattedDateString + '\n' +
+    var temp="***<没钱赚商店>购物清单***\n"+'打印时间:' + formattedDateString + '\n' +
             '----------------------\n' ;
     return temp;
 }
@@ -54,10 +54,10 @@ function toStandardList(inputs){
     for(i=0;i<fun.length;i++){//给所有商品增加count属性
     	fun[i].count=0;
         for(var j=0;j<inputs.length;j++){
-            if(inputs[j].indexOf("-")!=-1){
+            if(inputs[j].indexOf("-")!=-1){//检索带有"-"的对象,有的话分为两个,没有则返回-1;
 	        str=inputs[j].split('-');
                 if(str[0]==fun[i].barcode){
-                	fun[i].count=parseInt(str[1]);
+                	fun[i].count+=parseInt(str[1]);
                 }
             }
             else if (fun[i].barcode==inputs[j]){
@@ -83,26 +83,26 @@ function printList(array){
         var xiaoji=0;
         var zongji=0;
 	for(var i=0;i<array.length;i++){
-        if(array[i].count!=0){
+            if(array[i].count!=0){
             temp=temp+"名称:"+array[i].name+","+"数量:"+array[i].count+array[i].unit+","+"单价:"+array[i].price.toFixed(2)+"(元),小计:"+(array[i].price*(array[i].count-array[i].fut)).toFixed(2)+"(元)"+"\n";
         	zongji+=array[i].price*array[i].count;
+            }
+        }  
+        temp=temp+"----------------------"+"\n"+"挥泪赠送商品:"+"\n";
+        for(i=0;i<array.length;i++){
+            if(array[i].fut!=0){
+                temp=temp+"名称:"+array[i].name+",数量:"+array[i].fut+array[i].unit+"\n";
+                xiaoji+=array[i].price*array[i].fut;
+            }  
         }
-    }  
-    temp=temp+"----------------------"+"\n"+"挥泪赠送商品:"+"\n";
-    for(i=0;i<array.length;i++){
-        if(array[i].fut!=0){
-            temp=temp+"名称:"+array[i].name+",数量:"+array[i].fut+array[i].unit+"\n";
-            xiaoji+=array[i].price*array[i].fut;
-        }
-    }
-    zongji=zongji-xiaoji;
+        zongji=zongji-xiaoji;
    	temp=temp+"----------------------"+"\n";
-    temp=temp+"总计:"+zongji.toFixed(2)+"(元)"+"\n";
-    temp=temp+"节省:"+xiaoji.toFixed(2)+"(元)"+"\n";
-    temp=temp+"**********************";
-    return temp;
+        temp=temp+"总计:"+zongji.toFixed(2)+"(元)"+"\n";
+        temp=temp+"节省:"+xiaoji.toFixed(2)+"(元)"+"\n";
+        temp=temp+"**********************";
+        return temp;
     
-   }
+}
 
 function loadAllItems() {
     return [
